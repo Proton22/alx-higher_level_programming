c2515d1b55ed9ef6096af8e426e5e073101c5e24
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-09-23 13:28:28 +0100

    Task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    args = len(sys.argv)
-    j = 0
-    for i in range(1, args):
-        j += int(args[i])
-    print("{}".format(j))
+    jan = 0
+    for i in range(1, len(sys.argv)):
+        jan += int(sys.argv[i])
+    print("{}".format(jan))
