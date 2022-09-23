f0651b8ba65eeff5496830286c79654b9ee33ab9
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-09-23 13:04:15 +0100

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
         if args == 2:
             print("argument:")
         elif args > 2:
-             print("arguments:")
-             for i in range(args - 1):
-                  print("{}: {}".format((i + 1), sys.argv[i + 1]))
+            print("arguments:")
+        for i in range(args - 1):
+            print("{}: {}".format((i + 1), sys.argv[i + 1]))
