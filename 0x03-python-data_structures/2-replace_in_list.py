5bca60acc09c89d42892bdb38ebcf8ac12ff2c48
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-09-26 16:29:58 +0100

    Task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,8 +4,8 @@ def replace_in_list(my_list, idx, element):
         if idx == my_list.index(x):
             my_list.pop(idx)
             my_list.insert(idx, element)
-            return(my_list)
-    elif idx >= len(my_list):
-        return (my_list)
-    elif idx < 0:
-        return (my_list)
+            return (my_list)
+        elif idx >= len(my_list):
+            return (my_list)
+        elif idx < 0:
+            return (my_list)
