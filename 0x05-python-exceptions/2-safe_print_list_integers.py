60d02359b58f428c1e5de55f8de3ca47d6898316
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-10-11 12:46:12 +0100

    Task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,8 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]. end=""))
             count = count + 1
             print()
-    except OverflowError:
-        print("IndexError: list index out of range")
     except (TypeError, ValueError, IndexError):
-        print()
-        return (count)
+        pass
+    print()
+    return (count)
