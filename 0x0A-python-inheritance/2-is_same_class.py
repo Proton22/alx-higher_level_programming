234fe635931ba0fe7f68f34ff47826873ce631b3
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-10-25 00:41:24 +0100

    Task Done

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+"""
+    2-is_same_class: specified class
+"""
+def is_same_class(obj, a_class):
+    """
+       Returns True if the object is exactly an instance.
+       Args:
+           obj: object.
+           a_class: an instance of the specified class.
+       Returns:
+              True if the object is exactly the specified class
+              otherwise False
+    """
+    return isinstance(obj, a_class)
