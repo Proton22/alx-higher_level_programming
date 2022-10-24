baa482424120bc606749874ee20f648b33d45d19
Victor Chinyereugo <chinyereugovictor@gmail.com>
2022-10-24 23:46:13 +0100

    Task Done

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
+"""
+   0-lookup module
+"""
+
+
 def lookup(obj):
+    """
+        Returns the list of available attributes and methods.
+        Args:
+             obj: list of attribute.
+        Results:
+                lists of bojects.
+    """
     return(dir(obj))
